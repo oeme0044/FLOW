81bf45021b867cd0a7a2feb0d405256ab08652af
Joachim <joachim.berner@hotmail.com>
2021-10-13 13:52:55 +0200

    Fået de sidste bokse til Skills til at virke. og har fået bølgen til at virke.

--- skills.php
+++ skills.php
@@ -15,6 +15,7 @@
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
     <link href="css/skills.css" rel="stylesheet" type="text/css">
+    <link href="css/temp.scss" rel="stylesheet" type="text/css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
     <link rel="preconnect" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js">
@@ -31,71 +32,96 @@
 </head>
 
 <body>
+<?php include "header.php" ?>
 
-<div class="container p-5">
+<div class="container">
+    <div class="kort">
+        <div class="kort-titel">
+            <h1>Overskrift</h1>
+            <div class="linje1"></div>
+        </div>
+        <div class="kort-text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip.</p>
+        </div>
+    </div>
+</div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path fill="#ffffff" fill-opacity="1"
+          d="M0,96L48,90.7C96,85,192,75,288,58.7C384,43,480,21,576,32C672,43,768,85,864,117.3C960,149,1056,171,1152,165.3C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          class="position-relative"></path>
+</svg>
 
+
+<div class="bølge bg-white">
     <div>
-        <h2 class="p-5 text-white d-flex flex-wrap justify-content-center">Vores Kompetencer</h2>
-        <div class="linje"></div>
+        <h2 class="p-5 text-black d-flex flex-wrap justify-content-center">Vores Kompetencer</h2>
+        <p class="linje mt-3"></p>
     </div>
 
-    <div class="row ">
-        <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
-            <div class="card border-light p-1 p-md-4 mb-4 mb-lg-0">
-                <div class="card-body">
-                    <div class="icon icon-shape icon-shape-primary rounded-circle mb-4 d-flex justify-content-center">
-                        <i class="far fa-window-restore fa-3x text-black"></i>
+    <div class="container p-5">
+
+        <div class="row ">
+            <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
+                <div class="card border-light p-1 p-md-4 mb-4 mb-lg-0">
+                    <div class="card-body">
+                        <div class="icon icon-shape icon-shape-primary rounded-circle mb-4 d-flex justify-content-center">
+                            <i class="far fa-window-restore fa-3x text-white"></i>
+                        </div>
+                        <h5 class="mb-3 d-flex justify-content-center text-white">Web <span
+                                    class="text-lys-farve ms-1">Design</span>
+                        </h5>
+                        <p class="text-center text-white">Designed with the latest design trends in mind. Our product
+                            feels modern, creative, and
+                            beautiful.</p>
                     </div>
-                    <h5 class="mb-3 d-flex justify-content-center">Web <span class="text-lys-farve ms-1">Design</span>
-                    </h5>
-                    <p class="text-center">Designed with the latest design trends in mind. Our product
-                        feels modern, creative, and
-                        beautiful.</p>
                 </div>
             </div>
-        </div>
 
-        <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
-            <div class="card border-ra border-light p-1 p-md-4 mb-4 mb-lg-0">
-                <div class="card-body">
-                    <div class="icon icon-shape icon-shape-primary rounded-circle mb-4 d-flex justify-content-center">
-                        <i class="fas fa-camera fa-3x text-black"></i>
+            <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
+                <div class="card border-ra border-light p-1 p-md-4 mb-4 mb-lg-0">
+                    <div class="card-body">
+                        <div class="icon icon-shape icon-shape-primary rounded-circle mb-4 d-flex justify-content-center">
+                            <i class="fas fa-camera fa-3x text-black"></i>
+                        </div>
+                        <h5 class="mb-3 d-flex justify-content-center">Foto & <span
+                                    class="text-lys-farve ms-1">Video</span>
+                        </h5>
+                        <p class="text-center">Designed with the latest design trends in mind. Our product
+                            feels modern, creative, and
+                            beautiful.</p>
                     </div>
-                    <h5 class="mb-3 d-flex justify-content-center">Foto & <span class="text-lys-farve ms-1">Video</span>
-                    </h5>
-                    <p class="text-center">Designed with the latest design trends in mind. Our product
-                        feels modern, creative, and
-                        beautiful.</p>
                 </div>
             </div>
-        </div>
 
-        <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
-            <div class="card border-light p-1 p-md-4 mb-4 mb-lg-0">
-                <div class="card-body">
-                    <div class="icon icon-shape icon-shape-primary rounded-circle mb-4 d-flex justify-content-center">
-                        <i class="fas fa-film fa-3x text-black"></i>
+            <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
+                <div class="card border-light p-1 p-md-4 mb-4 mb-lg-0">
+                    <div class="card-body">
+                        <div class="icon icon-shape icon-shape-primary rounded-circle mb-4 d-flex justify-content-center">
+                            <i class="fas fa-film fa-3x text-black"></i>
+                        </div>
+                        <h5 class="mb-3 d-flex justify-content-center">Motion <span
+                                    class="text-lys-farve ms-1">Design</span></h5>
+                        <p class="text-center">Designed with the latest design trends in mind. Our product
+                            feels modern, creative, and
+                            beautiful.</p>
                     </div>
-                    <h5 class="mb-3 d-flex justify-content-center">Motion <span
-                                class="text-lys-farve ms-1">Design</span></h5>
-                    <p class="text-center">Designed with the latest design trends in mind. Our product
-                        feels modern, creative, and
-                        beautiful.</p>
                 </div>
             </div>
-        </div>
 
-        <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
-            <div class="card border-light p-1 p-md-4 mb-4 mb-lg-0">
-                <div class="card-body">
-                    <div class="icon icon-shape icon-shape-primary rounded-circle mb-4 d-flex justify-content-center">
-                        <i class="fas fa-pen-nib fa-3x text-black"></i>
+            <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
+                <div class="card border-light p-1 p-md-4 mb-4 mb-lg-0">
+                    <div class="card-body">
+                        <div class="icon icon-shape icon-shape-primary rounded-circle mb-4 d-flex justify-content-center">
+                            <i class="fas fa-pen-nib fa-3x text-black"></i>
+                        </div>
+                        <h5 class="mb-3 d-flex justify-content-center">Grafisk <span
+                                    class="text-lys-farve ms-1"> Design</span></h5>
+                        <p class="text-center">Designed with the latest design trends in mind. Our product
+                            feels modern, creative, and
+                            beautiful.</p>
                     </div>
-                    <h5 class="mb-3 d-flex justify-content-center">Grafisk <span
-                                class="text-lys-farve ms-1"> Design</span></h5>
-                    <p class="text-center">Designed with the latest design trends in mind. Our product
-                        feels modern, creative, and
-                        beautiful.</p>
                 </div>
             </div>
         </div>
@@ -103,6 +129,11 @@
 
 </div>
 
-<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+<svg xmlns="http://www.w3.org/2000/svg"    viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,96L48,90.7C96,85,192,75,288,58.7C384,43,480,21,576,32C672,43,768,85,864,117.3C960,149,1056,171,1152,165.3C1248,160,1344,128,1392,112L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+
+
+<script src=" node_modules
+    /bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
