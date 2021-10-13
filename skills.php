466a2c9bc3e3a7ecbcd4f2b63d272a6509c4cf9d
Joachim <joachim.berner@hotmail.com>
2021-10-13 12:42:10 +0200

    SKills

---
+++ skills.php
@@ -0,0 +1,111 @@
+<?php ?>
+
+
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Sigende titel</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js">
+    <link rel="preconnect" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="preconnect" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://kit.fontawesome.com/98690cc59f.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Red+Hat+Display:wght@700&display=swap"
+          rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<div class="container p-5">
+
+    <h2 class="p-5 text-white d-flex flex-wrap justify-content-center">Vores Kompetencer</h2>
+
+    <div class="row ">
+        <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
+            <div class="card border-light p-1 p-md-4 mb-4 mb-lg-0">
+                <div class="card-body">
+                    <div class="icon icon-shape icon-shape-primary rounded-circle mb-4 d-flex justify-content-center">
+                        <i class="fas fa-camera text-black"></i>
+                    </div>
+                    <h5 class="mb-3 d-flex justify-content-center">Web <span class="text-primary"> Design</span></h5>
+                    <p class="d-flex justify-items-center">Designed with the latest design trends in mind. Our product
+                        feels modern, creative, and
+                        beautiful.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
+            <div class="card border-light p-1 p-md-4 mb-4 mb-lg-0">
+                <div class="card-body">
+                    <div class="icon icon-shape icon-shape-primary rounded-circle mb-4 d-flex justify-content-center">
+                        <i class="fas fa-camera text-black"></i>
+                    </div>
+                    <h5 class="mb-3 d-flex justify-content-center">Web <span class="text-primary"> Design</span></h5>
+                    <p class="d-flex justify-items-center">Designed with the latest design trends in mind. Our product
+                        feels modern, creative, and
+                        beautiful.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
+            <div class="card border-light p-1 p-md-4 mb-4 mb-lg-0">
+                <div class="card-body">
+                    <div class="icon icon-shape icon-shape-primary rounded-circle mb-4 d-flex justify-content-center">
+                        <i class="fas fa-camera text-black"></i>
+                    </div>
+                    <h5 class="mb-3 d-flex justify-content-center">Web <span class="text-primary"> Design</span></h5>
+                    <p class="d-flex justify-items-center">Designed with the latest design trends in mind. Our product
+                        feels modern, creative, and
+                        beautiful.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 d-flex align-content-center flex-wrap rounded-2 ">
+            <div class="card">
+                <div class="card-body">
+                    <div class="mb-3 d-flex justify-content-center">
+                        <i class="fas fa-camera fa-3x text-black"></i>
+                    </div>
+                    <h5 class="mb-2 d-flex justify-content-center">Web <span class="text-primary"> Design</span></h5>
+                    <p class="d-flex justify-items-center">Designed with the latest design trends in mind. Our product
+                        feels modern, creative, and
+                        beautiful.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+        <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+
+        <!-- Force next columns to break to new line -->
+        <div class="w-100"></div>
+
+        <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+        <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+    </div>
+
+</div>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
