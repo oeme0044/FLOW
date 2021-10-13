3d3cb084049edb9d731fac445a753cef3f349377
TheGubbys <elmevej18denni@live.dk>
2021-10-13 10:59:21 +0200

    ændret navn på nav.php til header.php, tilføjet include på index.php, tilføjet test billede

--- header.php
+++ header.php
@@ -28,11 +28,26 @@
 
 <body>
 
-<nav class="navbar navbar-expand-md navbar-dark text-white bg-transparent sticky-top shadow">
+<nav class="navbar navbar-expand-sm navbar-dark text-white bg-transparent shadow">
     <div class="container">
         <a class="navbar-brand" href="index.php">
             <img src="./images/blueLogo.svg" alt="navLogo">
         </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+        </button>
+        <div class="collapse navbar-collapse flex-grow-0" id="navbarNav">
+            <ul class="navbar-nav text-right">
+                <li class="nav-item">
+                    <a class="nav-link active" href="about.php">Om Flow</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="contact.php">Kontakt</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
 
