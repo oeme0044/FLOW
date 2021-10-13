54dbcb11b1d6ce4efcd579dc38ece33fcf3f92a3
omertekbas <tekbas425@gmail.com>
2021-10-13 10:25:04 +0200

    footernav

---
+++ footer.php
@@ -0,0 +1,127 @@
+<!-- Instruktion til webbrowser om at vi kører HTML5 -->
+<!DOCTYPE html>
+
+<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
+<html lang="da">
+
+<!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
+<head>
+    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
+    <meta charset="utf-8">
+
+    <!-- Titel som ses oppe i browserens tab mv. -->
+    <title>Sigende titel</title>
+
+    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<!-- i <body> har man alt indhold på siden som brugeren kan se -->
+<body>
+
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Sigende titel</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/navfooter.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+
+
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="footer-widget">
+                    <h6>Ømer Tekbas</h6>
+                </div>
+                <p>My name is Ømer Tekbas. I am 19 years old. I live in Nykøbing Falster. I am currently studying to be a multimedia designer.</p>
+
+            </div>
+            <div class="col-lg-1"></div>
+
+            <div class="col-lg-2 col-md-2 col-sm-6">
+                <div class="footer-widget">
+                    <h6>Menu</h6>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">Skills</a></li>
+                        <li><a href="#">Contact</a></li>
+
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-3  col-md-4 col-sm-6">
+                <div class="footer-widget">
+                    <h6>Kontakt</h6>
+                    <ul>
+                        <li><i class="fas fa-mobile-alt"></i> +45 51 26 05 32</li>
+                        <li><i class="far fa-envelope"></i> tekbas425@gmail.com</li>
+                        <li><i class="fas fa-map-marker-alt"></i> Nykøbing Falster, Denmark</li>
+
+
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <h4>Sociale Medier</h4>
+                <div class="social-links">
+                    <a href="#"><i class="sm fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="sm fab fa-instagram"></i></a>
+                    <a href="#"><i class="sm fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <div class="footer-copyright-text">
+                <p>Copyright &copy; 2021 All rights reserved | Flow</p>
+            </div>
+        </div>
+    </div>
+
+
+    </div>
+
+</footer>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
+</body>
+</html>
