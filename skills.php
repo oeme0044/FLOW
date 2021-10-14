600739cf7671ea4723682cd6fba3109477c176f5
oliver <olivermarco97@gmail.com>
2021-10-14 13:46:21 +0200

    link til styling rettet

--- skills.php
+++ skills.php
@@ -15,7 +15,7 @@
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
     <link href="css/skills.css" rel="stylesheet" type="text/css">
-    <link href="css/temp.scss" rel="stylesheet" type="text/css">
+    <link href="css/temp.css" rel="stylesheet" type="text/css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
     <link rel="preconnect" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js">
@@ -51,7 +51,6 @@
 <br><br><br>
 
 
-
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
     <path fill="#ffffff" fill-opacity="1"
           d="M0,96L48,90.7C96,85,192,75,288,58.7C384,43,480,21,576,32C672,43,768,85,864,117.3C960,149,1056,171,1152,165.3C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
@@ -67,17 +66,19 @@
 
 
     <div class="d-flex justify-content-center">
-        <h1 class="p-2 text-black ">Vores Kompetencer</h1>
+        <h2 class="p-2 text-black justify-content-center">Vores Kompetencer</h2>
+
     </div>
+    <div  class="linje mt-3"></div>
 
-    <p class="linje mt-1"></p>
 
-    <div class="container">
+
+    <div class="container p-2">
 
         <div class="row ">
             <div class=" col-lg-3 col-md-6 d-flex align-content-center flex-wrap ">
-                <div class="card border-0 d-flex justify-content-evenly p-md-4 mb-4 mb-lg-0">
-                    <div class="  card-body ">
+                <div class="card d-flex justify-content-evenly border-0 p-1 p-md-4 mb-4 mb-lg-0">
+                    <div class=" w-50 p-3 card-body">
                         <div class="icon  mb-4 d-flex justify-content-center">
                             <i class="far fa-window-restore fa-3x text-white"></i>
                         </div>
@@ -90,7 +91,7 @@
             </div>
 
             <div class="col-lg-3 col-md-6 d-flex align-content-center flex-wrap ">
-                <div class="card border-0 d-flex justify-content-evenly p-md-4 mb-4 mb-lg-0">
+                <div class="card d-flex justify-content-evenly border-0 p-1 p-md-4 mb-4 mb-lg-0">
                     <div class="card-body">
                         <div class="icon mb-4 d-flex justify-content-center">
                             <i class="fas fa-camera fa-3x text-white"></i>
@@ -104,7 +105,7 @@
             </div>
 
             <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
-                <div class="card border-0 d-flex justify-content-evenly p-md-4 mb-4 mb-lg-0">
+                <div class="card d-flex justify-content-evenly border-0 p-1 p-md-4 mb-4 mb-lg-0">
                     <div class="card-body">
                         <div class="icon  mb-4 d-flex justify-content-center">
                             <i class="fas fa-film fa-3x text-white"></i>
@@ -117,7 +118,7 @@
             </div>
 
             <div class="col-lg-3 col-md-6  d-flex align-content-center flex-wrap ">
-                <div class="card border-0 d-flex justify-content-evenly p-md-4 mb-4 mb-lg-0">
+                <div class="card d-flex justify-content-evenly border-0 p-1 p-md-4 mb-4 mb-lg-0">
                     <div class="card-body">
                         <div class="icon  mb-4 d-flex justify-content-center">
                             <i class="fas fa-pen-nib fa-3x text-white"></i>
@@ -130,15 +131,38 @@
             </div>
         </div>
     </div>
-
-    <br><br>
-
-
 </div>
 
 
 
 <svg xmlns="http://www.w3.org/2000/svg"    viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,96L48,90.7C96,85,192,75,288,58.7C384,43,480,21,576,32C672,43,768,85,864,117.3C960,149,1056,171,1152,165.3C1248,160,1344,128,1392,112L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+
+
+
+
+    <div class="container padding_bt">
+        <div class="kort space">
+            <div class="kort-titel2 text-white text-center">
+                <h1>Tekst</h1>
+                <div class="linje2"></div>
+            </div>
+            <div class="kort-text text-white">
+                <p>Lidt om hvad coding og design er og gør.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path fill="#ffffff" fill-opacity="1"
+          d="M0,96L48,90.7C96,85,192,75,288,58.7C384,43,480,21,576,32C672,43,768,85,864,117.3C960,149,1056,171,1152,165.3C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          class="position-relative"></path>
+</svg>
+
+
+
+
 
 
 <script src=" node_modules
