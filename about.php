6483f8c5fadffa9af6c8f7917986e30672a9cce4
TheGubbys <elmevej18denni@live.dk>
2021-10-14 09:51:05 +0200

    fortsat med card

--- about.php
+++ about.php
@@ -31,22 +31,154 @@
 
 <body>
 
+<?php include 'header.php'; ?>
+<div>
+    <img class="hero" src="./images/test.jpg" alt="hero">
+    <div class="centered text-white"><h1>Om Flow</h1></div>
+</div>
+
 <div class="container">
-    <div class="col-md-6 p-5">
-        <div class="card rounder">
-            <img class="card-img-top rounder" src="images/profile/0.png">
-            <div class="p-2">
-                <p class="text-left text-muted">EXPERTOMRÅDE</p>
-                <h5 class="text-left card-title">NAVN EFTERNAVN</h5>
-                <p class="py-2 card-text d-sm-block d-none">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-                <div class="d-flex">
-                    <i class="far fa-envelope fa-2x thin"></i>
-                    <p class="my-auto px-2 d-xs-block d-none">Eksempel@gmail.com</p>
+    <div class="row">
+        <div class="col-6 col-lg-4 p-2"><!--Box 1-->
+            <div class="card rounder">
+                <img class="card-img-top rounder" src="images/profile/0.png">
+                <div class="p-2">
+                    <p class="text-left text-muted">EXPERTOMRÅDE</p>
+                    <h5 class="text-left card-title">NAVN EFTERNAVN</h5>
+                    <p class="py-2 card-text d-sm-block d-none">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <div class="row">
+                        <div class="col-12 col-sm-1">
+                            <i class="far fa-envelope fa-2x thin"></i>
+                        </div>
+                        <div class="col-12 col-sm-10 align-center my-auto">
+                            <p class="px-3 f-text">Eksempel@gmail.com</p>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                        </svg>
+                        <p class="my-auto px-2 f-text">88 88 88 88</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div> <!--Box1-->
+        <div class="col-6 col-lg-4 p-2"><!--Box 2-->
+            <div class="card rounder">
+                <img class="card-img-top rounder" src="images/profile/0.png">
+                <div class="p-2">
+                    <p class="text-left text-muted">EXPERTOMRÅDE</p>
+                    <h5 class="text-left card-title">NAVN EFTERNAVN</h5>
+                    <p class="py-2 card-text d-sm-block d-none">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <div class="d-flex">
+                        <i class="far fa-envelope fa-2x thin"></i>
+                        <p class="my-auto px-2 d-sm-block d-none">Eksempel@gmail.com</p>
+                    </div>
+                    <div class="d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                        </svg>
+                        <p class="my-auto px-2 d-sm-block d-none">88 88 88 88</p>
+                    </div>
+                </div>
+            </div>
+        </div> <!--Box2-->
+    </div>
+    <div class="row">
+        <div class="col-6 col-lg-4 p-2"><!--Box 3-->
+            <div class="card rounder">
+                <img class="card-img-top rounder" src="images/profile/0.png">
+                <div class="p-2">
+                    <p class="text-left text-muted">EXPERTOMRÅDE</p>
+                    <h5 class="text-left card-title">NAVN EFTERNAVN</h5>
+                    <p class="py-2 card-text d-sm-block d-none">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <div class="d-flex">
+                        <i class="far fa-envelope fa-2x thin"></i>
+                        <p class="my-auto px-2 f-text">Eksempel@gmail.com</p>
+                    </div>
+                    <div class="d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                        </svg>
+                        <p class="my-auto px-2 d-sm-block d-none">88 88 88 88</p>
+                    </div>
+                </div>
+            </div>
+        </div> <!--Box3-->
+        <div class="col-6 col-lg-4 p-2"><!--Box 4-->
+            <div class="card rounder">
+                <img class="card-img-top rounder" src="images/profile/0.png">
+                <div class="p-2">
+                    <p class="text-left text-muted">EXPERTOMRÅDE</p>
+                    <h5 class="text-left card-title">NAVN EFTERNAVN</h5>
+                    <p class="py-2 card-text d-sm-block d-none">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <div class="d-flex">
+                        <i class="far fa-envelope fa-2x thin"></i>
+                        <p class="my-auto px-2 d-sm-block d-none">Eksempel@gmail.com</p>
+                    </div>
+                    <div class="d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                        </svg>
+                        <p class="my-auto px-2 d-sm-block d-none">88 88 88 88</p>
+                    </div>
+                </div>
+            </div>
+        </div> <!--Box4-->
+    </div>
+    <div class="row">
+        <div class="col-6 col-lg-4 p-2"><!--Box 5-->
+            <div class="card rounder">
+                <img class="card-img-top rounder" src="images/profile/0.png">
+                <div class="p-2">
+                    <p class="text-left text-muted">EXPERTOMRÅDE</p>
+                    <h5 class="text-left card-title">NAVN EFTERNAVN</h5>
+                    <p class="py-2 card-text d-sm-block d-none">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <div class="d-flex">
+                        <i class="far fa-envelope fa-2x thin"></i>
+                        <p class="my-auto px-2 d-sm-block d-none">Eksempel@gmail.com</p>
+                    </div>
+                    <div class="d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                        </svg>
+                        <p class="my-auto px-2 d-sm-block d-none">88 88 88 88</p>
+                    </div>
+                </div>
+            </div>
+        </div> <!--Box5-->
+        <div class="col-6 col-lg-4 p-2"><!--Box 6-->
+            <div class="card rounder">
+                <img class="card-img-top rounder" src="images/profile/0.png">
+                <div class="p-2">
+                    <p class="text-left text-muted">EXPERTOMRÅDE</p>
+                    <h5 class="text-left card-title">NAVN EFTERNAVN</h5>
+                    <p class="py-2 card-text d-sm-block d-none">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <div class="d-flex">
+                        <i class="far fa-envelope fa-2x thin"></i>
+                        <p class="my-auto px-2 d-sm-block d-none">Eksempel@gmail.com</p>
+                    </div>
+                    <div class="d-flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                        </svg>
+                        <p class="my-auto px-2 d-sm-block d-none">88 88 88 88</p>
+                    </div>
+                </div>
+            </div>
+        </div> <!--Box6-->
     </div>
 </div>
 
