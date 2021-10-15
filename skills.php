97fb31e473edbce7b69671f65e368ed8b8d9264d
Joachim <joachim.berner@hotmail.com>
2021-10-15 10:34:41 +0200

    Included nogle filer til index.php så vores forside virker.

--- skills.php
+++ skills.php
@@ -16,6 +16,7 @@
     <link href="css/styles.css" rel="stylesheet" type="text/css">
     <link href="css/skills.css" rel="stylesheet" type="text/css">
     <link href="css/temp.css" rel="stylesheet" type="text/css">
+    <link href="css/header.css" rel="stylesheet" type="text/css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
     <link rel="preconnect" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js">
